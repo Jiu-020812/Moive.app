ad7f7b22a3abb2102f80b0dfe94f4b1c9b562f22
지우 <a35107471@gmail.com>
2025-09-19 03:22:59 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import
+{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+import Home from"./routes/home"
 
 function App() {
-  console.log("✅ App render");
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([])
-  const getMovies = async() => {
-    const response = await fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year');
-    const json = await response.json();
+  return <Router>
+  <Switch>
+    <Route path="/">
+    <Home />
 
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-  <div>
-    {loading ? (<h1>Loading...</h1>)
-    : (movies.map (movie => <div key={movie.id}>{movie.title}</div>))}
-    </div>
-);
+    </Route>
+  </Switch>
+  </Router>;
 }
-
 
 export default App
